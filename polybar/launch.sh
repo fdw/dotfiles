6f51826c769eb0eb927f78d8914117d0fd37de73
Fabian Winter <fdw@users.noreply.github.com>
2018-03-31 10:39:14 +0200

    polybar: Less modules, more pretty
    I usually don't care about the system's temperature or the CPU load, so
    why show it all the time. Now it's just what I'm interested in, and the
    signal/noise ratio is improved.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,9 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-MONITOR=eDP-1 polybar mybar &
-MONITOR=HDMI-1 polybar mybar &
-MONITOR=VGA-1 polybar mybar
-
-echo "Bars launched..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload powerbar &
+  done
+else
+  polybar --reload powerbar &
+fi
