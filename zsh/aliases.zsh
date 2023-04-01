0309f73aa94ae4c6c778ee70096eba52427c3dac
Fabian Winter <fdw@users.noreply.github.com>
2023-04-01 09:26:52 +0200

    zsh: Update aliases
    I've never used the fancy git functions, but I use docker a lot now

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,16 +1,3 @@
 alias g='git'
 alias v='nvim'
-
-function gswitch() {
-  local branches branch
-  branches=$(git branch --all | grep -v HEAD) &&
-    branch=$(echo "$branches" | fzf -d $(( 2 + $(wc -l <<< "$branches") )) +m --height=10) &&
-    git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-  }
-
-function gf() {
-  branch=$(git rev-parse --abbrev-ref HEAD)
-  commit=$(git log --oneline origin/master.."$branch" | fzf | awk '{print $1}')
-
-  git commit --fixup="$commit"
-}
+alias d='docker'
